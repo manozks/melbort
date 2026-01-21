ff4e011019dcbc929f86e8f4cbc2545818ccd8f1
manoj <manozraz@yahoo.com>
2026-01-21 14:38:04 +0545

    footer section updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -38,30 +38,28 @@ const Footer = () => {
                 <li><Link to="/" className="hover:text-[#d4af37] transition-colors">Home</Link></li>
                 <li><a href="/#services" className="hover:text-[#d4af37] transition-colors">Services</a></li>
                 <li><a href="/#about" className="hover:text-[#d4af37] transition-colors">About Us</a></li>
-                <li><a href="/#partners" className="hover:text-[#d4af37] transition-colors">Partners</a></li>
-                <li><a href="/#contact" className="hover:text-[#d4af37] transition-colors">Contact</a></li>
-                <li><Link to="/privacy-policy" className="hover:text-[#d4af37]">Privacy Policy</Link></li>
-              </ul>
+                <li><a href="/#partners" className="hover:text-[#d4af37] transition-colors">Partners</a></li>                
+                <li><Link to="/privacy-policy" className="hover:text-[#d4af37]">Privacy Policy</Link></li>              </ul>
             </div>
 
             {/* 3. Contact Details */}
            <div className="relative z-10">
-             <h5 className="font-bold text-gray-900 mb-6">Contact Us</h5>
+             <h5 className="font-bold text-gray-900 mb-6">Location</h5>
             <ul className="space-y-4 text-sm text-black-600">
               <li className="flex items-start gap-3">
                 <MapPin size={18} className="text-[#d4af37] mt-1 shrink-0" />
                 <span>Level 1, 123 Digital Drive<br/>Sydney, NSW 2000<br/>Australia</span>
               </li>
-              <li className="flex items-center gap-3">
+             {/*  <li className="flex items-center gap-3">
                 <Phone size={18} className="text-[#d4af37] shrink-0" />
                 <a href="https://wa.me/61419616922" target="_blank"  rel="noopener noreferrer"
     className="hover:text-[#d4af37] transition-colors cursor-pointer"
   >+61 419 616 922</a>                
-              </li>
-              <li className="flex items-center gap-3">
+              </li> */}
+              {/* <li className="flex items-center gap-3">
                 <Mail size={18} className="text-[#d4af37] shrink-0" />
                 <span>info@melbote.com</span>
-              </li>
+              </li> */}
             </ul>
            </div>
 
